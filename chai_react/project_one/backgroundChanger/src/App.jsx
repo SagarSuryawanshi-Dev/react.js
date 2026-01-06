f4e25f86bcf8de5105d74b72326d6ded15680a5f
Sagar <sagarsurya45@gmail.com>
2026-01-06 18:03:37 +0530

    bgChanger

--- chai_react/project_one/backgroundChanger/src/App.jsx
+++ chai_react/project_one/backgroundChanger/src/App.jsx
@@ -1,34 +1,32 @@
- import { useState } from "react"
+import { useState } from "react"
 function App() {
-   const [color, setColor] = useState("olive")
+  const [color, setColor] = useState("olive")
 
   return (
-   
-   <div className="h-screen w-full duration-800"
-    style={{backgroundColor: color}}
 
-    
-   >
-    <div className=" fixed justify-center bottom-10 border-2 flex flex-wrap inset-x-2 ">
-    
-    <div className="flex flex-wrap justify-center shadow-xl bg-white p-2 rounded-full gap-3">
-      <button className="outline-none bg-red p-4"
-      onClick={()=> setColor("red")}
-      style={{backgroundColor:"red"}}
-      >RED</button>
-      <button className="outline-none bg-red p-4"
-      style={{backgroundColor:"green"}}
-       onClick={()=> setColor("green")}
-      >Grren</button>
-      <button className="outline-none bg-red p-4"
-      style={{backgroundColor:"pink"}}
-       onClick={()=> setColor("pink")}
-      >pink</button>
-    </div>
-    </div>
-   </div> 
-   
+    <div className="h-screen w-full duration-800"
+      style={{ backgroundColor: color }}
 
+
+    >
+      <div className="flex fixed justify-center bottom-10 flex-wrap inset-x-2 ">
+
+        <div className="flex flex-wrap justify-center shadow-xl bg-white p-2 rounded-full gap-3 bo">
+          <button className="outline-none p-4 rounded-full"
+            onClick={() => setColor("red")}
+            style={{ backgroundColor: "red" }}
+          >RED</button>
+          <button className="outline-none p-4 rounded-full"
+            style={{ backgroundColor: "green" }}
+            onClick={() => setColor("green")}
+          >Grren</button>
+          <button className="outline-none rounded-full p-4"
+            style={{ backgroundColor: "pink" }}
+            onClick={() => setColor("pink")}
+          >pink</button>
+        </div>
+      </div>
+    </div>
   )
 }
 

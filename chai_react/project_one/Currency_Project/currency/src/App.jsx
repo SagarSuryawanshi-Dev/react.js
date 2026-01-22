38feb2e586d95529cc4ccb5c070cd9a739a2e935
Sagar <sagarsurya45@gmail.com>
2026-01-22 17:56:45 +0530

    currency converter

---
+++ chai_react/project_one/Currency_Project/currency/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-red-500 rounded-lg text-white p-6 border-2 border-black font-bold">
+      Tailwind Test
+    </div>
+  )
+}
+export default App

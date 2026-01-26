d6cf8571fa0b253b8512198d39a44c456fd10876
Sagar <sagarsurya45@gmail.com>
2026-01-26 17:10:27 +0530

    currencyConverter

--- chai_react/project_one/Currency_Project/currency/src/components/Input.jsx
+++ chai_react/project_one/Currency_Project/currency/src/components/Input.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useId} from 'react'
 
 function InputBox({
     label,
@@ -13,32 +13,37 @@ function InputBox({
     
 }) {
    
-
+    const ammountInputId = useId()
     return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex `}>
+        <div className={`bg-white p-3 rounded-lg text-sm flex  border-2 text-black`}>
             <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
-                   { label}
+                <label htmlFor={ammountInputId} className="text-black/40 mb-2 inline-block border-2">
+                   {label}
                 </label>
                 <input
-                    
-                    className="outline-none w-full bg-transparent py-1.5"
+                    id={ammountInputId}
+                    className="outline-none w-full bg-transparent py-1.5 border-2"
                     type="number"
                     placeholder="Amount"
                     disabled={ammountDisable}
                     value={ammount}
+                    onChange={(e)=>onAmmountChange && onAmmountChange(Number(e.target.value)) }
                 />
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    
+                    value={selectCurrency}
+                    onChange={(e)=> onCurrencyChange && onCurrencyChange(e.target.value)}
+                    disabled = {currencyDisable}
                 >
                     
-                        <option value="usd">
-                            usd
-                        </option>
+                        {currencyOptions.map((currency)=> (
+                            <option key={currency} value ={currency}>
+                                {currency }
+                            </option>
+                        ))}
                 
                 </select>
             </div>
---
+++ chai_react/project_one/Currency_Project/currency/src/components/index.js
@@ -0,0 +1,7 @@
+// all componets imports here and from here we exports 
+import Input from "./Input";
+
+
+// export
+
+export {Input}
